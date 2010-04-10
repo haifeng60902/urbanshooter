8cf0ab1c5c314f05943c509670e42296800fae15
ange3d <ange3d@410222e6-29fe-11df-9952-ebc1c0ccc740>
2010-04-10 17:27:01 +0000

    begin of weapon implementation

---
+++ src/render/WeaponHandler.h
@@ -0,0 +1,24 @@
+#ifndef _WEAPON_HANDLER_H_
+#define _WEAPON_HANDLER_H_
+
+
+#include <osgGA/GUIEventHandler>
+
+
+
+class WeaponHandler : public osgGA::GUIEventHandler 
+{
+public:
+
+	bool handle(const osgGA::GUIEventAdapter& ea, osgGA::GUIActionAdapter& aa);
+
+
+
+};
+
+
+
+
+
+#endif //_WEAPON_HANDLER_H_
+
