99f40811633c8b534f472b926fd331af15369cec
ange3d <ange3d@410222e6-29fe-11df-9952-ebc1c0ccc740>
2011-03-17 21:38:06 +0000

    Creation of the library

---
+++ Hud/src/CallbackList.cpp
@@ -0,0 +1,82 @@
+#include <animation/CallbackList.h>
+
+CallbackList::CallbackList()
+{
+	_running  = false;
+}
+
+CallbackList::~CallbackList()
+{
+
+}
+
+void CallbackList::operator()(osg::Node* node, osg::NodeVisitor* nv)
+{
+
+	_running = true;
+
+	//call each callback of the list
+	for(CallbackCallList::iterator it = _callbackList.begin(); it != _callbackList.end() ; ++it)
+	{
+		if((*it).second.valid())
+			(*((*it).second.get()))(node, nv);
+	}
+
+	_running  = false;
+
+	//continue graph traverse
+	traverse(node, nv);
+
+
+	//flush callback that auto-delete themselves
+	flushWaitingElementToRemove();
+}
+
+
+void CallbackList::addCallback( osg::NodeCallback * callback, int index)
+{
+	if( index < 0 || index >= (int)_callbackList.size() )
+	{
+		_callbackList.push_back(std::make_pair(callback->className(), callback));
+	}
+	else
+	{
+		//put iterator on next element before insertion
+		CallbackCallList::iterator it = _callbackList.begin() + index;
+		_callbackList.insert(it , std::make_pair(callback->className(), callback));
+	}
+}
+
+void CallbackList::removeCallback( const std::string & name, bool remove_all_instances)
+{
+	//stack the entity to be removed
+	_remStack.push_back( std::make_pair( name, remove_all_instances ) );
+
+	if(!_running)
+	{
+		flushWaitingElementToRemove();
+	}
+}
+
+
+void CallbackList::flushWaitingElementToRemove()
+{
+	//for each element of the wating list, remove one or all its instances
+	for(RemoveEntityStack::iterator its = _remStack.begin(); its != _remStack.end(); ++its)
+		for(CallbackCallList::iterator it = _callbackList.begin(); it != _callbackList.end() ; )
+			{
+				if(it->first.compare(its->first) == 0)
+				{
+					//no incrementation in this case
+					it = _callbackList.erase(it);
+					
+					if(!its->second)
+						break;
+				}
+				else
+					++it;
+			}
+
+
+	_remStack.clear();
+}
---
+++ Hud/src/CallbackList.h
@@ -0,0 +1,59 @@
+#ifndef _CALLBACK_LIST_H_
+#define _CALLBACK_LIST_H_
+
+
+#include <utility/Macros.h>
+
+#include <osg/Node>
+#include <string>
+#include <vector>
+
+/** Implements a simple callback able to call a list of NodeCallback.
+ *	This is a tool to allow having more than one callback on a simple node
+ */
+class CallbackList : public osg::NodeCallback
+{
+public:
+
+	CallbackList();
+	~CallbackList();
+
+	//not mandatory, but keep consistency
+	METADATA(CallbackList);
+
+
+	/** operator of the callback */
+	void operator()(osg::Node* node, osg::NodeVisitor* nv);
+
+	/** Add a new callaback 
+	 *	@param[in] callback nodeCallback to add
+	 *	@param[in] index index where to insert the callback. default (-1) is in queue
+	 */
+	void addCallback( osg::NodeCallback * callback, int index = -1);
+
+	/** Remove an existing callback 
+	 *	@param[in] name name of the callback (will call className())
+	 *	@param[in] remove_all_instances remove all instances of the callback if true, else just the first one
+	 */
+	void removeCallback( const std::string & name, bool remove_all_instances = false);
+
+private:
+
+	/**	Remove callbacks that are waiting for beeing removed */
+	void flushWaitingElementToRemove();
+
+	/** List of callbacks based on the className */
+	typedef std::vector< std::pair< std::string, osg::ref_ptr< osg::NodeCallback > > > CallbackCallList;
+	CallbackCallList _callbackList;
+
+	/** List of elements wating to be removed */
+	typedef std::vector< std::pair< std::string, bool > > RemoveEntityStack;
+	RemoveEntityStack _remStack;
+	
+	/** callback list is beeing run */
+	bool _running;
+
+};
+
+#endif //_CALLBACK_LIST_H_
+
