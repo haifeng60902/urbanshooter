69249e521ff33b12dc068a2332471b3a7cfa6384
ange3d <ange3d@410222e6-29fe-11df-9952-ebc1c0ccc740>
2011-11-06 09:16:53 +0000

    State hud update : almost done callback to update texts is implemented

--- trunk/Shooter/sources/graphic/StateHud.cpp
+++ trunk/Shooter/sources/graphic/StateHud.cpp
@@ -9,6 +9,11 @@
 
 StateHud::StateHud()
 {
+
+	_scoreI = new int();
+	_chronoI = new int();
+	_bulletI = new int();
+
 	// set the projection matrix
     setProjectionMatrix(osg::Matrix::ortho2D(0,1280,0,1024));
 	
@@ -27,6 +32,9 @@ StateHud::StateHud()
 
 
 	createAmoGeometry();
+
+	//add the draw callback for multi-htread rendering issue
+	_totalBullet->setDrawCallback(new StateHudDrawCallback(_totalBullet, _score, _chrono, _bulletI, _scoreI, _chronoI));
 }
 
 StateHud::~StateHud()
@@ -89,7 +97,7 @@ void StateHud::createAmoGeometry()
 	//create the total amo text zone
 
 	_totalBullet = new osgText::Text();
-	_totalBullet->setPosition(osg::Vec3(maxX-width+margin,maxY-margin*10,-0.5)); //4th point
+	_totalBullet->setPosition(osg::Vec3(maxX-width+margin,maxY-margin*9,-0.5)); //4th point
 	_totalBullet->setAutoRotateToScreen(false);
 	_totalBullet->setCharacterSizeMode(osgText::TextBase::SCREEN_COORDS);
 	_totalBullet->setCharacterSize(120);
@@ -127,6 +135,86 @@ void StateHud::createAmoGeometry()
 	}
 
 
+
+	//create the chrono text
+
+	_chrono = new osgText::Text();
+	_chrono->setPosition(osg::Vec3(maxX-width-height+margin,maxY-margin*9, -0.5)); //4th point
+	_chrono->setAutoRotateToScreen(false);
+	_chrono->setCharacterSizeMode(osgText::TextBase::SCREEN_COORDS);
+	_chrono->setCharacterSize(100);
+	_chrono->setText("Remaining Time: 920");
+	_chrono->setColor(osg::Vec4(textcolor, 1.0));
+
+
+	osg::Geometry* geomchrono = new osg::Geometry;
+	{
+		osg::Vec3Array* vertices = new osg::Vec3Array();
+		
+		//horizontal square
+		vertices->push_back(osg::Vec3(maxX-width-height+margin,maxY-margin*5,-0.5));
+		vertices->push_back(osg::Vec3(maxX-width-height+margin*30,maxY-margin*5,-0.5));
+		vertices->push_back(osg::Vec3(maxX-width-height+margin*30,maxY-margin*10,-0.5));
+		vertices->push_back(osg::Vec3(maxX-width-height+margin,maxY-margin*10,-0.5));
+
+		geomchrono->setVertexArray(vertices);
+
+		osg::Vec3Array* normals = new osg::Vec3Array;
+		normals->push_back(osg::Vec3(0.0f,0.0f,1.0f));
+		geomchrono->setNormalArray(normals);
+		geomchrono->setNormalBinding(osg::Geometry::BIND_OVERALL);
+
+		osg::Vec4Array* colors = new osg::Vec4Array;
+		colors->push_back(osg::Vec4(linecolor, 1.0));
+		geomchrono->setColorArray(colors);
+		geomchrono->setColorBinding(osg::Geometry::BIND_OVERALL);
+
+		geomchrono->addPrimitiveSet(new osg::DrawArrays(GL_LINE_LOOP,0,vertices->size()));
+		//set width
+		osg::LineWidth * ln = new osg::LineWidth;
+		ln->setWidth(lineWidth);
+		geomchrono->getOrCreateStateSet()->setAttributeAndModes(ln, osg::StateAttribute::ON);
+	}
+
+
+
+	_score = new osgText::Text();
+	_score->setPosition(osg::Vec3(maxX-width-height+margin*35,maxY-margin*9,-0.5)); //4th point
+	_score->setAutoRotateToScreen(false);
+	_score->setCharacterSizeMode(osgText::TextBase::SCREEN_COORDS);
+	_score->setCharacterSize(100);
+	_score->setText("Score: 1535454");
+	_score->setColor(osg::Vec4(textcolor, 1.0));
+
+	osg::Geometry* geomscore = new osg::Geometry;
+	{
+		osg::Vec3Array* vertices = new osg::Vec3Array();
+		
+		//horizontal square
+		vertices->push_back(osg::Vec3(maxX-width-height+margin*35,maxY-margin*5,-0.5));
+		vertices->push_back(osg::Vec3(maxX-width-height+margin*55,maxY-margin*5,-0.5));
+		vertices->push_back(osg::Vec3(maxX-width-height+margin*55,maxY-margin*10,-0.5));
+		vertices->push_back(osg::Vec3(maxX-width-height+margin*35,maxY-margin*10,-0.5));
+
+		geomscore->setVertexArray(vertices);
+
+		osg::Vec3Array* normals = new osg::Vec3Array;
+		normals->push_back(osg::Vec3(0.0f,0.0f,1.0f));
+		geomscore->setNormalArray(normals);
+		geomscore->setNormalBinding(osg::Geometry::BIND_OVERALL);
+
+		osg::Vec4Array* colors = new osg::Vec4Array;
+		colors->push_back(osg::Vec4(linecolor, 1.0));
+		geomscore->setColorArray(colors);
+		geomscore->setColorBinding(osg::Geometry::BIND_OVERALL);
+
+		geomscore->addPrimitiveSet(new osg::DrawArrays(GL_LINE_LOOP,0,vertices->size()));
+		//set width
+		osg::LineWidth * ln = new osg::LineWidth;
+		ln->setWidth(lineWidth);
+		geomscore->getOrCreateStateSet()->setAttributeAndModes(ln, osg::StateAttribute::ON);
+	}
+
 	//disable culling
 	geode->setCullingActive(false);
 	//disable picking
@@ -138,7 +226,11 @@ void StateHud::createAmoGeometry()
 	geode->addDrawable(geomBG);
 	//add textLines
 	geode->addDrawable(geomText);
+	geode->addDrawable(geomchrono);
+	geode->addDrawable(geomscore);
 	geode->addDrawable(_totalBullet);
+	geode->addDrawable(_chrono);
+	geode->addDrawable(_score);
 
 	//attach to the camera
 	addChild(geode);
--- trunk/Shooter/sources/graphic/StateHud.h
+++ trunk/Shooter/sources/graphic/StateHud.h
@@ -4,6 +4,8 @@
 #include <osg/Camera>
 #include <osgText/Text>
 
+#include <sstream>
+
 class Weapon;
 
 //class for amo hud and scores
@@ -14,16 +16,80 @@ public:
 	StateHud();
 	~StateHud();
 
+	int setScore(int score){ *_scoreI = score; }
+	int setchrono(int chrono){ *_chronoI = chrono; }
+	int setTotalBullet(int bullet){ *_bulletI = bullet; }
+
 private:
 
 	void createAmoGeometry();
 
 	Weapon * _weapon; //use a setter that will rebuild/refresh the 
 
-	//text for amo total
-	//callback de MAJ ou alors une notification directe
-
 	osg::ref_ptr<osgText::Text> _totalBullet;
+	osg::ref_ptr<osgText::Text> _chrono;
+	osg::ref_ptr<osgText::Text> _score;
+
+	int * _scoreI;
+	int * _chronoI;
+	int * _bulletI;
+	
+
+
+	//private update text callback to avoid crashes
+
+
+	
+
+	class StateHudDrawCallback : public virtual osg::Drawable::DrawCallback
+	{
+	private :
+	        // See HUD3D declaration to have the index-Text matching
+	      
+		osg::ref_ptr<osgText::Text> _totalBullet;
+		osg::ref_ptr<osgText::Text> _chrono;
+		osg::ref_ptr<osgText::Text> _score;
+
+		int * _scoreI;
+		int * _chronoI;
+		int * _bulletI;
+	
+	public :
+		StateHudDrawCallback(osgText::Text* tbullet, osgText::Text* tscore, osgText::Text* tchrono, int * ibullet, int * iscore, int * ichrono):
+		   _totalBullet( tbullet ),
+		   _chrono( tchrono ),
+		   _score( tscore ),
+		   _bulletI( ibullet ),
+		   _scoreI( iscore ),
+		   _chronoI( ichrono )
+	        {
+	        }
+	
+	private :
+	        virtual void drawImplementation(osg::RenderInfo& renderInfo,const osg::Drawable* drawable) const
+	        {
+	            //osg::ref_ptr<osgText::Text> text = (osgText::Text*)drawable;
+				_totalBullet->setText( "Amo: " + to_string( *_bulletI ) );
+				_chrono->setText( "Remaining Time: " + to_string( *_chronoI ) );
+				_score->setText( "Score: " + to_string( *_scoreI ) );
+		
+				_totalBullet->drawImplementation(renderInfo);
+				_chrono->drawImplementation(renderInfo);
+				_score->drawImplementation(renderInfo);
+	        }
+
+			static std::string to_string( int i)
+			{
+				 // créer un flux de sortie
+				std::ostringstream oss;
+				// écrire un nombre dans le flux
+				oss << i;
+				// récupérer une chaîne de caractères
+				std::string result = oss.str();
+				return result;
+			}
+	}; 
+	
 };
 
 #endif //_STATE_HUD_H_
